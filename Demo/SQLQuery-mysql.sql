6c6d5f4af1d419621045f835fe529c55c1a01f3f
lyz05 <liuyuanzhe0515@gmail.com>
2020-01-16 13:08:23 +0800

    添加mysql数据库的脚本

--- Demo/SQLQuery-mysql.sql
+++ Demo/SQLQuery-mysql.sql
@@ -1,5 +1,5 @@
 /*创建数据库（BookDB）*/
-CREATE DATABASE BookDB
+CREATE DATABASE BookDB;
 
 /*转到（BookDB）数据库*/
 go
@@ -10,7 +10,7 @@ CREATE TABLE AdminUsers
 (
 	username char(8) primary key,
 	password char(255) not null
-)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Book
 (
@@ -21,18 +21,18 @@ CREATE TABLE Book
 	price decimal(7,2) not null,
 	publishingDate date not null,
 	shopNum int not null
-)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Reader
 (
 	readerNO char(8) primary key,
 	readerName char(8) not null,
 	sex char(2) not null check(sex='男' or sex='女'),
-	identitycard char(18) not null check(len(identitycard)=18),
+	identitycard char(18) not null check(length(identitycard)=18),
 	workUnit char(50) not null,
 --	borrowCount tinyint,
 	password char(255) not null
-)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Borrow
 (
@@ -43,7 +43,7 @@ CREATE TABLE Borrow
 	returnDate date,
 	foreign key(bookNO) references Book(bookNO),
 	foreign key(readerNO) references Reader(readerNO)
-)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
 
 /* 为JDBC连接创建用户 */
 go
@@ -52,7 +52,7 @@ exec sp_adduser 'BookDB','BookDB'
 exec sp_addrolemember 'db_owner', 'BookDB'
 
 /* 修改基本表约束 */
-alter table Borrow add constraint CK_time check(borrowDate<=shouldDate)
+alter table Borrow add constraint CK_time check(borrowDate<=shouldDate);
 
 /* 创建视图 */
 --借阅未还视图
@@ -108,16 +108,16 @@ go
 insert AdminUsers values('admin','');
 
 /*读者表（Reader）数据：*/
-INSERT INTO Reader VALUES('R2005001','张小娟','女','412723199001014321','统一股份有限公司','')
-INSERT INTO Reader VALUES('R2006001','刘凤','女','412723199003014321','联合股份有限公司','')
-INSERT INTO Reader VALUES('R2007001','高代鹏','男','412723199005014321','洪都股份有限公司','')
-INSERT INTO Reader VALUES('R2008001','陈辉','男','412723199111014321','南昌市电脑研制公司','')
-INSERT INTO Reader VALUES('R2009001','李虹冰','女','412723199208014321','富士康科技集团','')
-INSERT INTO Reader VALUES('R2005002','张露','女','412723199002014321','兴隆股份有限公司','')
-INSERT INTO Reader VALUES('R2006002','喻自强','男','412723199004014321','万事达股份有限公司','')
-INSERT INTO Reader VALUES('R2007002','张晓梅','女','412723199112014321','世界技术开发公司','')
-INSERT INTO Reader VALUES('R2008002','张良','男','412723199110014321','上海生物研究室','')
-INSERT INTO Reader VALUES('R2009002','韩福平','男','412723199209014321','合生元有限公司','')
+INSERT INTO Reader VALUES('R2005001','张小娟','女','412723199001014321','统一股份有限公司','');
+INSERT INTO Reader VALUES('R2006001','刘凤','女','412723199003014321','联合股份有限公司','');
+INSERT INTO Reader VALUES('R2007001','高代鹏','男','412723199005014321','洪都股份有限公司','');
+INSERT INTO Reader VALUES('R2008001','陈辉','男','412723199111014321','南昌市电脑研制公司','');
+INSERT INTO Reader VALUES('R2009001','李虹冰','女','412723199208014321','富士康科技集团','');
+INSERT INTO Reader VALUES('R2005002','张露','女','412723199002014321','兴隆股份有限公司','');
+INSERT INTO Reader VALUES('R2006002','喻自强','男','412723199004014321','万事达股份有限公司','');
+INSERT INTO Reader VALUES('R2007002','张晓梅','女','412723199112014321','世界技术开发公司','');
+INSERT INTO Reader VALUES('R2008002','张良','男','412723199110014321','上海生物研究室','');
+INSERT INTO Reader VALUES('R2009002','韩福平','男','412723199209014321','合生元有限公司','');
 
 /*图书表(Book)数据：*/
 INSERT INTO Book VALUES('B200101001','政治经济学','宋涛','中国人民大学出版社',31.80,'19910101',5);
@@ -132,26 +132,26 @@ INSERT INTO Book VALUES('B200301003','人工智能','费翔林','电子工业出版社',43.20,
 INSERT INTO Book VALUES('B200301004','算法设计与分析','陈慧南','科学出版社',58.20,'19930104',10);
 
 /*借阅表（Borrow）数据：*/
-INSERT INTO Borrow VALUES('R2005001','B200101001','20110901','20111001',null)
-INSERT INTO Borrow VALUES('R2006001','B200101001','20110907','20111007','20111005')
-INSERT INTO Borrow VALUES('R2007001','B200201001','20110913','20111013','20111010')
-INSERT INTO Borrow VALUES('R2008001','B200301001','20110915','20111015','20111013')
-INSERT INTO Borrow VALUES('R2009001','B200301001','20110918','20111018',null)
-INSERT INTO Borrow VALUES('R2005001','B200101002','20110902','20111002',null)
-INSERT INTO Borrow VALUES('R2005001','B200101003','20110903','20111003',null)
-INSERT INTO Borrow VALUES('R2005002','B200101001','20110904','20111004','20111003')
-INSERT INTO Borrow VALUES('R2005002','B200101002','20110905','20111005',null)
-INSERT INTO Borrow VALUES('R2005002','B200101003','20110906','20111006','20111008')
-INSERT INTO Borrow VALUES('R2006001','B200101002','20110908','20111008','20111006')
-INSERT INTO Borrow VALUES('R2006001','B200101003','20110909','20111009',null)
-INSERT INTO Borrow VALUES('R2009002','B200201001','20110910','20111010','20111008')
-INSERT INTO Borrow VALUES('R2006002','B200201002','20110911','20111011','20111008')
-INSERT INTO Borrow VALUES('R2006002','B200201003','20110912','20111012','20111008')
-INSERT INTO Borrow VALUES('R2007001','B200201002','20110914','20111014',null)
-INSERT INTO Borrow VALUES('R2007001','B200201003','20110915','20111015','20111010')
-INSERT INTO Borrow VALUES('R2009002','B200201001','20110916','20111016','20111014')
-INSERT INTO Borrow VALUES('R2007002','B200201002','20110917','20111017','20111014')
-INSERT INTO Borrow VALUES('R2007002','B200201003','20110917','20111017','20111014')
+INSERT INTO Borrow VALUES('R2005001','B200101001','20110901','20111001',null);
+INSERT INTO Borrow VALUES('R2006001','B200101001','20110907','20111007','20111005');
+INSERT INTO Borrow VALUES('R2007001','B200201001','20110913','20111013','20111010');
+INSERT INTO Borrow VALUES('R2008001','B200301001','20110915','20111015','20111013');
+INSERT INTO Borrow VALUES('R2009001','B200301001','20110918','20111018',null);
+INSERT INTO Borrow VALUES('R2005001','B200101002','20110902','20111002',null);
+INSERT INTO Borrow VALUES('R2005001','B200101003','20110903','20111003',null);
+INSERT INTO Borrow VALUES('R2005002','B200101001','20110904','20111004','20111003');
+INSERT INTO Borrow VALUES('R2005002','B200101002','20110905','20111005',null);
+INSERT INTO Borrow VALUES('R2005002','B200101003','20110906','20111006','20111008');
+INSERT INTO Borrow VALUES('R2006001','B200101002','20110908','20111008','20111006');
+INSERT INTO Borrow VALUES('R2006001','B200101003','20110909','20111009',null);
+INSERT INTO Borrow VALUES('R2009002','B200201001','20110910','20111010','20111008');
+INSERT INTO Borrow VALUES('R2006002','B200201002','20110911','20111011','20111008');
+INSERT INTO Borrow VALUES('R2006002','B200201003','20110912','20111012','20111008');
+INSERT INTO Borrow VALUES('R2007001','B200201002','20110914','20111014',null);
+INSERT INTO Borrow VALUES('R2007001','B200201003','20110915','20111015','20111010');
+INSERT INTO Borrow VALUES('R2009002','B200201001','20110916','20111016','20111014');
+INSERT INTO Borrow VALUES('R2007002','B200201002','20110917','20111017','20111014');
+INSERT INTO Borrow VALUES('R2007002','B200201003','20110917','20111017','20111014');
 
 /* 删除BookDB数据库及用户 */
 go
@@ -173,17 +173,16 @@ select * from View_Book where 图书编号 like '%2001%' and 图书名称 like '%经济%' 
 -- 借书
 select * from Borrow where readerNO = 'R2009001' and bookNO='B200201003' and returnDate is null
 if exists(select * from View_Book where 图书编号='B200201003' and 在库数量>0)
-insert Borrow values('R2006002','B200201003',getdate(),dateadd(mm,1,getdate()),null)
+insert Borrow values('R2006002','B200201003',now() ,date_add(NOW(), interval 1 MONTH) ,null)
 -- 还书
-update Borrow set returnDate=getdate() from borrow
-where readerNO = 'R2009001' and bookNO='B200201003' and returnDate is null
+update Borrow set returnDate=now() where readerNO='R2006001' and bookNO='B200101003' and returnDate is null;
 -- 续借
-update Borrow set shouldDate=dateadd(mm,1,getdate()) from Borrow
+update Borrow set shouldDate=date_add(NOW(), interval 1 MONTH)
 where readerNO = 'R2009001' and bookNO='B200201003' and returnDate is null
 
 /* 管理员读者信息窗口操作 */
 -- 重置密码
-update Reader set password='' from Reader
+update Reader set password=''
 where readerNo = 'R2009001'
 -- 添加
 insert Reader values('R2010001','李雷','男','442000199001014321','北京理工大学珠海学院','')
@@ -215,7 +214,7 @@ select readerNO from Reader where readerNo='R2005001' and password=''
 
 /* 修改密码窗口 */
 select readerNO from Reader where readerNo='R2005001' and password=''
-update Reader set password=encodeInp(pwd) from Reader where readerNo = ''
+update Reader set password=encodeInp(pwd) where readerNo = ''
 
 /* 测试用 */
 delete from Borrow where bookNO='B200301002'
